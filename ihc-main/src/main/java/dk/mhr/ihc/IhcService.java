0a637bdacf3d60ae7435b3258ca5aa62052a90c9
Morten Rummelhoff <morten@mhr.dk>
2016-03-17 21:09:59 +0100

    Updated Websocket to also sending messages to client

--- ihc-main/src/main/java/dk/mhr/ihc/IhcService.java
+++ ihc-main/src/main/java/dk/mhr/ihc/IhcService.java
@@ -1,8 +1,16 @@
 package dk.mhr.ihc;
 
+import dk.mhr.ihc.entity.DataMessage;
+import dk.mhr.ihc.entity.IhcData;
 import dk.mhr.ihc.wsdl.cxf.OpenAPIService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import java.util.Date;
 
 /**
  * Created by mortenrummelhoff on 14/03/16.
@@ -10,9 +18,13 @@ import org.springframework.stereotype.Component;
 @Component
 public class IhcService {
 
+    private Logger logger = LoggerFactory.getLogger(getClass());
 
     @Autowired
     private OpenAPIService openAPIService;
+
+    @Autowired
+    private SimpMessagingTemplate template;
 
     public void ping() {
      openAPIService.ping();
@@ -21,5 +33,16 @@ public class IhcService {
     public Integer getApiVersion() {
         return openAPIService.getAPIVersion();
     }
+
+
+    @Scheduled(fixedRate = 5000)
+    public void reportCurrentTime() {
+        broadCast(new DataMessage("Hvad sker der bliver der broadCasted"));
+    }
+
+    public void broadCast(DataMessage message) {
+        template.convertAndSend("/topic/message", new DataMessage(message.getMessage()));
+    }
+
 
 }
